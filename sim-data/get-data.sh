bf55d6197adfb6b5fccbc4950841826f90e7df95
Philipp Girichidis <philipp@girichidis.com>
2026-07-08 14:44:52 +0200

    added lower resolution file

--- sim-data/get-data.sh
+++ sim-data/get-data.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#for f in SILCC_hdf5_plt_cnt_3000-uniform-cube-512.pkl SILCC_hdf5_plt_cnt_3000-uniform-cube-512.pkl SILCC_hdf5_plt_cnt_3000
-for f in SILCC_hdf5_plt_cnt_3000-uniform-cube-512.pkl SILCC_hdf5_plt_cnt_3000-uniform-cube-512.pkl
+#for f in SILCC_hdf5_plt_cnt_3000-uniform-cube-N512.pkl SILCC_hdf5_plt_cnt_3000-uniform-cube-N256-full.pkl SILCC_hdf5_plt_cnt_3000
+for f in SILCC_hdf5_plt_cnt_3000-uniform-cube-N512.pkl SILCC_hdf5_plt_cnt_3000-uniform-cube-N256-full.pkl
 do
-   if [ ! -e ]
+   if [ ! -e $f ]
    then
       echo "downloading: $f"
-      wget datentaxi.de/13/les-houches-2026/$f -O $f
+      echo wget datentaxi.de/13/les-houches-2026/$f -O $f
    else
       echo "file $f already there!"
    fi
