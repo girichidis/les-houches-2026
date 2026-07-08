f4070c5d66bc7db209be0bf09479880424b00e1b
Philipp Girichidis <philipp@girichidis.com>
2026-07-08 14:29:25 +0200

    included 256^3 with all data

--- sim-data/get-data.sh
+++ sim-data/get-data.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-for f in SILCC_hdf5_plt_cnt_3000-uniform-cube-512.pkl SILCC_hdf5_plt_cnt_3000
+#for f in SILCC_hdf5_plt_cnt_3000-uniform-cube-512.pkl SILCC_hdf5_plt_cnt_3000-uniform-cube-512.pkl SILCC_hdf5_plt_cnt_3000
+for f in SILCC_hdf5_plt_cnt_3000-uniform-cube-512.pkl SILCC_hdf5_plt_cnt_3000-uniform-cube-512.pkl
 do
    if [ ! -e ]
    then
       echo "downloading: $f"
-      wegt blu4:www/13/les-houches-2026/$f -O $f
+      wget datentaxi.de/13/les-houches-2026/$f -O $f
    else
       echo "file $f already there!"
    fi
